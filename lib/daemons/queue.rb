93e9488ac4d7b2b217c0e157a4c9710ab6f3bd51
Anton Grebnev <anton.grebnev@netmonsters.ru>
2013-02-27 17:58:06 +0400

    added queue daemon

--- lib/daemons/queue.rb
+++ lib/daemons/queue.rb
@@ -17,8 +17,12 @@ end
 while($running) do
   
   # Replace this with your code
-  Rails.logger.auto_flushing = true
   Rails.logger.info "This daemon is still running at #{Time.now}.\n"
-  
-  sleep 10
+
+  raw = RestClient.get("http://www.premierleague.com/content/premierleague/en-gb/matchday/matches/2012-2013/epl.match-report.html/man-city-vs-liverpool")
+  html = Nokogiri::HTML(raw)
+  Rails.logger.info "Start parsing"
+  Rails.logger.info html.css("ul.idx li.stats a")[0]["href"]
+
+  sleep 3
 end
