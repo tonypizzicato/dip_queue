fcca80b825eaf4773682eb372e7a0ff0fffbd9cc
Anton Grebnev <anton.grebnev@netmonsters.ru>
2013-06-09 17:44:49 +0400

    moved specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
+# This file is copied to spec/ when you run 'rails generate spec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
@@ -27,7 +27,7 @@ RSpec.configure do |config|
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
-  # rspec-rails.
+  # spec-rails.
   config.infer_base_class_for_anonymous_controllers = false
 
   # Run specs in random order to surface order dependencies. If you find an
